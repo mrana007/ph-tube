383dcc17914635eff0b216a4396359835d44bf02
mrana007 <rana.es.bc@gmail.com>
2023-09-01 00:08:17 +0600

    added video container dynamically

--- script/index.js
+++ script/index.js
@@ -1,9 +1,7 @@
 const handleCategory = async() =>{
-    
-
+    // category section
     const response = await fetch('https://openapi.programming-hero.com/api/videos/categories');
     const data = await response.json();
-
     const tabContainer = document.getElementById('tab-container');
     data.data.forEach((category) => {
         const div = document.createElement('div');
@@ -15,10 +13,40 @@ const handleCategory = async() =>{
 
     // console.log(data.data);
 };
+// videos section
 const handleLoadVideo = async (categoryId) => {
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`)
     const data = await response.json();
-    console.log(data);
+    const videoContainer = document.getElementById('video-container');
+    data.data.forEach((videos) =>{
+        // console.log(videos);
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div class = "card bg-base-100">
+        <figure>
+            <img class= "h-52" src = ${videos?.thumbnail}>
+        </figure>
+        <div class = "card-body">
+            <h2 class = "card-title">
+            <div class = "avatar">
+                <div class = "w-14 rounded-full">
+                    <img src = ${videos?.authors[0].profile_picture}>
+                </div>
+            </div>
+                ${videos.title}
+            </h2>
+            <div>
+                <h5>${videos?.authors[0].profile_name}
+                ${videos?.authors[0].verified}
+                </h5>
+                <small>${videos?.others.views}</small>
+            </div>
+        </div>
+        </div>
+        `;
+        videoContainer.appendChild(div);
+    })
+    // console.log(data.data);
 };
 
 handleCategory();
